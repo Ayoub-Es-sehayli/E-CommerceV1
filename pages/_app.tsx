8012bc40bd3c577f02bc3a6bac296ccd0b2f3eb8
Es-sehayli Ayoub <ayoub1essehayli@outlook.com>
2022-10-09 09:47:46 +0100

    Initial Project State
    Removed poiler-plate code
    Added the packages necessary to build the ui
    Cleaned Up the folder structure for feature based workflow

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "store";
+import { ReactElement, ReactNode } from "react";
+import { NextPage } from "next";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+  return (
+    <Provider store={store}>{getLayout(<Component {...pageProps} />)}</Provider>
+  );
 }
-
-export default MyApp
