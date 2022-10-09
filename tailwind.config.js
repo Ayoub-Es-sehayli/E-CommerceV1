8859cfa431806defdaf70f567360e2fea60fe4f2
Es-sehayli Ayoub <ayoub1essehayli@outlook.com>
2022-10-09 10:16:57 +0100

    Configured the base design system
    Tailwind configuration for theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,25 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ["Noto Sans", "sans-serif"],
+      serif: ["Noto Serif", "serif"],
+    },
+    colors: {
+      transparent: "transparent",
+      white: "hsl(0 0% 100%)",
+      black: "hsl(0 0% 12%)",
+      grey: {
+        200: "hsl(174 18% 39%/ 0.68)",
+        400: "hsl9174 18% 39%/ 0.68)",
+      },
+      primary: {
+        200: "hsl(174, 83%, 39%)",
+        400: "hsl(174, 85%, 18%)",
+        600: "hsl(175, 85%, 8%)",
+      },
+      accent: "hsl(356, 79%, 60%)",
+    },
     extend: {},
   },
   plugins: [],
