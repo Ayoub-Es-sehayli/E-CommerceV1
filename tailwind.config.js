d47c93ff417b8cb104e32174507d80bf558d5928
Es-sehayli Ayoub <ayoub1essehayli@outlook.com>
2022-10-10 00:51:52 +0100

    Added Some packages for UI building
    Icon pack: Bootstrap Icons
    Styling: Tailwind CSS + DaisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./features/**/*.{js,ts,jsx,tsx}"],
+  plugins: [require("daisyui")],
   theme: {
     fontFamily: {
       sans: ["Noto Sans", "sans-serif"],
       serif: ["Noto Serif", "serif"],
     },
-    colors: {
-      transparent: "transparent",
-      white: "hsl(0 0% 100%)",
-      black: "hsl(0 0% 12%)",
-      grey: {
-        200: "hsl(174 18% 39%/ 0.68)",
-        400: "hsl9174 18% 39%/ 0.68)",
-      },
-      primary: {
-        200: "hsl(174, 83%, 39%)",
-        400: "hsl(174, 85%, 18%)",
-        600: "hsl(175, 85%, 8%)",
-      },
-      accent: "hsl(356, 79%, 60%)",
+    screens: {
+      sm: "425px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
     },
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        white: "hsl(0 0% 100%)",
+        light: "hsl(37 18% 83% / 0.87)",
+        black: "hsl(0 0% 12%)",
+        grey: {
+          200: "hsl(0 0% 0% / 0.1)",
+          400: "hsl(174 18% 39% / 0.68)",
+        },
+        primary: {
+          200: "hsl(174, 83%, 39%)",
+          400: "hsl(174, 85%, 18%)",
+          600: "hsl(175, 85%, 8%)",
+        },
+        accent: "hsl(356, 79%, 60%)",
+      },
+    },
   },
-  plugins: [],
+  daisyui: {
+    styled: false,
+    themes: false,
+    base: false,
+    utils: false,
+    logs: true,
+    rtl: false,
+    prefix: "daisy-",
+  },
 };
