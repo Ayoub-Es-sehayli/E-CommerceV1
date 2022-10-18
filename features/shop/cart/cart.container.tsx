c51bfa931a47d1bed778bf1f18a9544b31b0419f
Es-sehayli Ayoub <ayoub1essehayli@outlook.com>
2022-10-18 18:12:15 +0100

    Finished refactoring the hook code

--- features/shop/cart/cart.container.tsx
+++ features/shop/cart/cart.container.tsx
@@ -1,38 +1,23 @@
 import { Form, Formik, FormikHelpers } from "formik";
 import CartItemsList from "./cart-items-list.component";
 import CheckoutForm from "./checkout-form.component";
-import CheckoutFormModel, { CheckoutFormSchema } from "./checkout.schema";
+import { CheckoutFormSchema } from "./checkout.schema";
 import useCart from "./useCart";
 
 export default function CartContainer() {
-  const { items: cartItems, total } = useCart();
-  const initialValues: CheckoutFormModel = {
-    items: cartItems.map<{ id: string; quantity: number }>((item) => {
-      return { id: item.id, quantity: item.quantity };
-    }),
-    shipping: {
-      address: "",
-      city: "",
-    },
-  };
-  const handleSubmit = (
-    values: CheckoutFormModel,
-    { setSubmitting }: FormikHelpers<CheckoutFormModel>
-  ) => {
-    console.log(values);
-
-    setSubmitting(true);
-  };
+  const { items: cartItems, total, handleSubmit, initialValues } = useCart();
   return (
     <Formik
       initialValues={initialValues}
       onSubmit={handleSubmit}
       validationSchema={CheckoutFormSchema}
     >
-      <Form className="flex flex-col md:flex-row lg:justify-center gap-4">
-        <CartItemsList items={cartItems} total={total} />
-        <CheckoutForm />
-      </Form>
+      {({ isSubmitting }) => (
+        <Form className="flex flex-col md:flex-row lg:justify-center gap-4">
+          <CartItemsList items={cartItems} total={total} />
+          <CheckoutForm submitting={isSubmitting} />
+        </Form>
+      )}
     </Formik>
   );
 }
