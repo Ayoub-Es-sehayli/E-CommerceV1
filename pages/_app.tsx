599095c8952f10d6ba1245eb0b7d7532ec76349b
Es-sehayli Ayoub <ayoub1essehayli@outlook.com>
2022-10-19 21:55:41 +0100

    Added routing to algolia
    To add a history while navigating the product list in the shop

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { Provider } from "react-redux";
-import { store } from "store";
-import { ReactElement, ReactNode } from "react";
 import { NextPage } from "next";
+import type { AppProps } from "next/app";
+import { ReactElement, ReactNode } from "react";
 import { InstantSearch } from "react-instantsearch-hooks-web";
+import { Provider } from "react-redux";
 import { algoliaSearchClient } from "services/algolia-service";
+import { store } from "store";
+import "../styles/globals.css";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -19,7 +19,11 @@ type AppPropsWithLayout = AppProps & {
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
   return (
-    <InstantSearch searchClient={algoliaSearchClient} indexName="dev_products">
+    <InstantSearch
+      searchClient={algoliaSearchClient}
+      indexName="dev_products"
+      routing
+    >
       <Provider store={store}>
         {getLayout(<Component {...pageProps} />)}
       </Provider>
