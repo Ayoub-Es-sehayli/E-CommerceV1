e915641f14b41a07329cd434623ffc08c3da7ae9
Es-sehayli Ayoub <ayoub1essehayli@outlook.com>
2022-11-21 21:52:32 +0100

    Moved Algolia Context to the shoplayout

--- features/ui/shop.layout.tsx
+++ features/ui/shop.layout.tsx
@@ -1,4 +1,10 @@
+import { useRouter } from "next/router";
 import { PropsWithChildren } from "react";
+import { InstantSearch } from "react-instantsearch-hooks-web";
+import {
+  algoliaRoutingConfig,
+  algoliaSearchClient,
+} from "services/algolia-service";
 import Footer from "./footer.component";
 import NavBar from "./navbar/navbar.component";
 import { SideBar } from "./navbar/sidebar.component";
@@ -6,21 +12,34 @@ import useUILoaders from "./useUILoaders.hook";
 
 export default function ShopLayout({ children }: PropsWithChildren) {
   const { isLoading } = useUILoaders();
+  const router = useRouter();
   return (
-    <div className="daisy-drawer">
-      <input id="nav-drawer" type="checkbox" className="daisy-drawer-toggle" />
-      <div className="daisy-drawer-content h-screen flex flex-col justify-between">
-        <header>
-          <NavBar
-            attributes={["category.lvl0", "category.lvl1", "category.lvl2"]}
-          />
-        </header>
-        <main className="container mx-auto mb-4 lg:my-4 px-4">{children}</main>
-        <Footer />
+    <InstantSearch
+      searchClient={algoliaSearchClient}
+      indexName="dev_products"
+      routing={algoliaRoutingConfig(router)}
+    >
+      <div className="daisy-drawer">
+        <input
+          id="nav-drawer"
+          type="checkbox"
+          className="daisy-drawer-toggle"
+        />
+        <div className="daisy-drawer-content h-screen flex flex-col justify-between">
+          <header>
+            <NavBar
+              attributes={["category.lvl0", "category.lvl1", "category.lvl2"]}
+            />
+          </header>
+          <main className="container mx-auto mb-4 lg:my-4 px-4">
+            {children}
+          </main>
+          <Footer />
+        </div>
+        <SideBar
+          attributes={["category.lvl0", "category.lvl1", "category.lvl2"]}
+        />
       </div>
-      <SideBar
-        attributes={["category.lvl0", "category.lvl1", "category.lvl2"]}
-      />
-    </div>
+    </InstantSearch>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
 import { ReactElement, ReactNode } from "react";
-import { InstantSearch } from "react-instantsearch-hooks-web";
 import { Provider } from "react-redux";
-import {
-  algoliaRoutingConfig,
-  algoliaSearchClient,
-} from "services/algolia-service";
 import { store } from "store";
 import "../styles/globals.css";
-
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
@@ -20,21 +14,22 @@ export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
-  const router = useRouter();
   return (
     <QueryClientProvider client={queryClient}>
-      <InstantSearch
-        searchClient={algoliaSearchClient}
-        indexName="dev_products"
-        routing={algoliaRoutingConfig(router)}
-      >
-        <Provider store={store}>
-          {getLayout(<Component {...pageProps} />)}
-        </Provider>
-      </InstantSearch>
+      <Provider store={store}>
+        {getLayout(<Component {...pageProps} />)}
+      </Provider>
+
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
