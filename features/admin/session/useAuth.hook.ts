a758f3596fbf433fb007bfc45ad06ae8dd77371c
Es-sehayli Ayoub <ayoub1essehayli@outlook.com>
2022-11-25 21:30:13 +0100

    Added authorization of the admin on a navigation level

--- features/admin/session/useAuth.hook.ts
+++ features/admin/session/useAuth.hook.ts
@@ -6,42 +6,49 @@ import {
   signOut,
 } from "firebase/auth";
 import { collection, doc, getDoc } from "firebase/firestore";
+import { useRouter } from "next/router";
 import { useState } from "react";
-import {
-  firebaseAuth,
-  firebaseDb,
-  googleProvider,
-} from "services/firebase-service";
+import { firebaseAuth, firebaseDb } from "services/firebase-service";
 import UserConverter from "./user.converter";
 
 export default function useAuth() {
   const { isLoggedIn } = useSession();
   const [unauthorized, setUnauthorized] = useState(false);
+  const router = useRouter();
+
   const handleGoogleSignIn = async () => {
-    await signInWithPopup(firebaseAuth, googleProvider).then((result) => {
-      if (result) {
-        const credentials = GoogleAuthProvider.credentialFromResult(result);
-        if (credentials) {
-          signInWithCredential(firebaseAuth, credentials)
-            .then(async ({ user }) => {
-              const userSnap = await getDoc(
-                doc(firebaseDb, "users", user.uid).withConverter(UserConverter)
-              );
-              if (userSnap.exists() && userSnap.data().admin) {
-                console.log("Authentication Sucessfull");
-              } else {
-                signOut(firebaseAuth);
-                setUnauthorized(true);
-              }
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-        } else {
-          console.error("Failed to authenticate user");
+    const googleProvider = new GoogleAuthProvider();
+    await signInWithPopup(firebaseAuth, googleProvider)
+      .then((result) => {
+        if (result) {
+          const credentials = GoogleAuthProvider.credentialFromResult(result);
+          if (credentials) {
+            signInWithCredential(firebaseAuth, credentials)
+              .then(async ({ user }) => {
+                const userSnap = await getDoc(
+                  doc(firebaseDb, "users", user.uid).withConverter(
+                    UserConverter
+                  )
+                );
+                if (userSnap.exists() && userSnap.data().admin) {
+                  console.log("Authentication Sucessfull");
+                  router.push("/admin/orders");
+                } else {
+                  signOut(firebaseAuth);
+                  setUnauthorized(true);
+                }
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          } else {
+            console.error("Failed to authenticate user");
+          }
         }
-      }
-    });
+      })
+      .catch((error) => {
+        console.error("Failed to authenticate user");
+      });
   };
 
   return { handleGoogleSignIn, unauthorized };
--- features/admin/ui/admin.layout.tsx
+++ features/admin/ui/admin.layout.tsx
@@ -1,4 +1,5 @@
 import useSession from "@features/session/useSession.hook";
+import Spinner from "@features/ui/spinner.component";
 import useUILoaders from "@features/ui/useUILoaders.hook";
 import { useRouter } from "next/router";
 import { PropsWithChildren } from "react";
@@ -9,16 +10,17 @@ export default function AdminLayout({ children }: PropsWithChildren) {
   const { isLoading } = useUILoaders();
   const { isLoggedIn } = useSession();
   const router = useRouter();
-  if (!isLoggedIn) {
-    router.replace("/session");
+  if (router.isReady && !isLoggedIn) {
+    router.replace("/admin");
   }
   return (
     <div className="h-screen bg-light-200 font-serif">
+      {/* <span className="hidden text-warning "></span> */}
       <Header />
       <div className="flex px-8 py-4 gap-4 w-screen">
         <Navigation />
         <main className="bg-white rounded-lg p-4 font-sans w-full">
-          {children}
+          {isLoading ? <Spinner isLoading /> : children}
         </main>
       </div>
     </div>
